3ee25dfe6bc41a33890453e89ceb6883fce92f20
Juliette Engelaere-Lefebvre <juliette.engelaere@developpement-durable.gouv.fr>
2022-06-17 15:24:49 +0200

    Un peu de ménage vers z_archives Ajout dans le support du catalogue d'attributs de la base insee qui s'est perdu depuis bien des lustres (rien trouvé comme xls sur github bien que remontée jusque 2016...) Ajout de titre au chunck au début de chapitre pour que la compilation du book soit plus bavarde Ajout de la fonction pour créer le book pdf

--- m1_admin.R
+++ m1_admin.R
@@ -5,3 +5,27 @@ bookdown::render_book("index.Rmd", "bookdown::epub_book")
 
 # Erreur à la compilation
 # bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+pagedown::chrome_print(input = "index.rmd")
+
+# Commande pour générer un support pdf du module
+propre.rpls::creer_pdf_book(
+  chemin_book = "_book/", nom_pdf = "M1_Debuter_avec_R_et_RStudio.pdf",
+  pages_html = c(
+    "index.html",
+    "bien-commencer.html",
+    "package-ggplot2.html",
+    "le-mapping.html",
+    "les-formes-géométriques.html",
+    "lhabillage-simple.html",
+    "les-thèmes.html",
+    "les-scales.html",
+    "la-mise-en-page-de-plusieurs-graphiques.html",
+    "les-facettes.html",
+    "exporter-un-graphique.html",
+    "créer-des-cartes-avec-ggplot2.html",
+    "animer-ses-graphiques-ggplot-avec-gganimate.html",
+    "créer-des-graphiques-et-cartes-pour-le-web.html",
+    "créer-des-tableaux-avec-kable-et-kable-extra.html",
+    "ressources.html",
+    "exercices-et-corrections.html"
+  ))
