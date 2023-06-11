243279569b553bb4a588bb1741a2526dd4080fdb
Juliette Engelaere-Lefebvre <37416795+jengelaere@users.noreply.github.com>
2023-06-11 02:15:11 +0200

    Dev (#39)
    * début review
    
    * fin relecture, et implémentation des exercices à partir de savoirfR
    
    * Correction encodage csv
    
    * oubli dépendance savoirFR dans DESCRIPTION

--- m1_admin.R
+++ m1_admin.R
@@ -20,8 +20,7 @@ propre.rpls::creer_pdf_book(
     "premiers-graphiques.html",                                    
     "sauvegarder-son-travail.html" ,
     "aller-plus-loin-avec-les-objets-et-la-programmation-fonctionnelle.html",
-    "exercices-corrigés.html",                                    
-    "exercices-pour-r-studio.html"
+    "exercices-corrigés.html"
   ))
 
 imports <- sort(c("bookdown", attachment::att_from_rmds(path = ".", pattern = ".Rmd", recursive = FALSE, inline = TRUE)))
