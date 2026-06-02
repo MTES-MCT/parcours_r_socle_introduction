73568651bf73dfb407baf123bf410c78c3c14a0f
juliette.engelaere <juliette.engelaere@developpement-durable.gouv.fr>
2026-06-02 13:43:36 +0200

    Debug Déploiement CI : nouvelle tentative d'y voir clair - en autorisant le build knitr à poursuivre malgré les erreurs pour voir les résultats Error de compilation difficile à reproduire et impossible à comprendre: 110/159 [graph_3] Error in `ggplot(data = base, mapping = aes(x = log_SUPERF)) + geom_histogram()`: ! non-numeric argument to binary operator

--- m1_admin.R
+++ m1_admin.R
@@ -1,6 +1,6 @@
 # Commande pour générer à la main le book du module 1
 
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+bookdown::render_book("index.Rmd", quiet = FALSE)
 bookdown::render_book("index.Rmd", "bookdown::epub_book")
 
 # Erreur à la compilation
